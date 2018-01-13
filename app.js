d30d838cc3ab80cfbb9b64fe28a730e2b91e6c78
Jonathan Phipps <jonathanmp25@gmail.com>
2018-01-13 13:55:15 -0600

    Add media queries

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'));
 
-const server = app.listen(3000, () => {
+const server = app.listen(80, () => {
     const host = server.address().address
     const port = server.address().port
     console.log('Server Started');
